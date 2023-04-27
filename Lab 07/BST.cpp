fdca5e3deabaf644aa2349195105c6c39177a376
Thejas0604 <109301978+Thejas0604@users.noreply.github.com>
2023-04-27 14:42:39 +0530

    Deleted the folder

--- Lab 07/BST.cpp
+++ Lab 07/BST.cpp
@@ -8,17 +8,33 @@ struct node {
 
 // Inorder traversal
 void traverseInOrder(struct node *root) {
-
+  if (root ==NULL){
+    return;
+  }
+  traverseInOrder(root->left);
+  cout<< root->key<<" ";
+  traverseInOrder(root ->right);
 }
 
 // Insert a node
 struct node *insertNode(struct node *node, int key) {
-
+  if(node==NULL){}
+    node = new struct node();
+    node->key = key;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+  if(key>node->key){
+    node->right = insertNode(node->right,key);
+  }else if(key<=node->key){
+    node->left = insertNode(node->left,key);
+  }
+  return node;
 }
 
 // Deleting a node
 struct node *deleteNode(struct node *root, int key) {
- 
+  
 }
 
 // Driver code
